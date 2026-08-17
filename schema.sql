7af32daee96ac82375110ac43942d92080551b66
orpsisdope <orpsisdopeaf@gmail.com>
2026-08-17 23:57:39 +0200

    second_commit

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE IF NOT EXISTS items (
     title VARCHAR(100) NOT NULL,
     type VARCHAR(10) NOT NULL CHECK (type IN ('lost', 'found'))
 );
+
+
+
+INSERT INTO items
+(title, type, category, location, item_date, description, contact)
+SELECT
+    'Black backpack',
+    'lost'
