e07670043060589cd8ec447f4bde542213e08d88
orpsisdope <orpsisdopeaf@gmail.com>
2026-08-18 16:00:53 +0200

    add_pool_config_with_connection_string

--- db.js
+++ db.js
@@ -3,3 +3,7 @@ const { Pool } = require("pg");
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is missing. Add it to your .env file.");
 }
+
+const poolConfig = {
+  connectionString: process.env.DATABASE_URL
+};
