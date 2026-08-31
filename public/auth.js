1e8eea7a36e157b5f4fe178608b74bee5d87b1e8
orpsisdope <orpsisdopeaf@gmail.com>
2026-08-31 13:38:03 +0200

    Add_client_side_signup_and_login_logic_using_fetch_API

---
+++ public/auth.js
@@ -0,0 +1,77 @@
+async function signup(){
+
+const response =
+await fetch("/api/auth/signup",
+{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+
+username:
+username.value,
+
+email:
+email.value,
+
+password:
+password.value
+
+})
+
+});
+
+
+alert("Account created");
+
+}
+
+
+
+async function login(){
+
+
+const response =
+await fetch("/api/auth/login",
+{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+email:
+email.value,
+
+password:
+password.value
+
+})
+
+});
+
+
+const data =
+await response.json();
+
+
+localStorage.setItem(
+"token",
+data.token
+);
+
+
+localStorage.setItem(
+"username",
+data.username
+);
+
+
+window.location="index.html";
+
+
+}
